e2f9c787286bf2463180a9a2258dfc98eadbeee0
Kerry Jiang <kerry-jiang@hotmail.com>
2019-05-18 22:19:04 -0700

    more classes

--- src/Matplotlib.Net/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Matplotlib.Net
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Matplotlib.Net/PyPlot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Python.Runtime;
+
+namespace Matplotlib.Net
+{
+    public class PyPlot
+    {
+        public float[] YValues { get; private set; }
+
+        public float[] XValues { get; private set; }
+
+        public PyPlot()
+        {
+
+        }
+
+        public PyPlot X(float[] values)
+        {
+            XValues = values;
+            return this;
+        }
+
+        public PyPlot Y(float[] values)
+        {
+            YValues = values;
+            return this;
+        }
+
+        public object Show()
+        {
+            using (Py.GIL())
+            {
+                dynamic mpl = Py.Import("matplotlib");
+                dynamic plt = Py.Import("matplotlib.pyplot");
+
+                plt.plot(XValues, YValues);
+                plt.show();
+            }
+
+            return null;
+        }
+    }
+}
